ee6debe0cee2f5ff9e275d45836e5ff71846fec8
wzz <13731160065@users.noreply.github.com>
2018-03-28 01:02:21 +0800

    Update checkp3.sh

--- checkp3.sh
+++ checkp3.sh
@@ -5,23 +5,26 @@ else
 	echo "未找到该文件，你是不是忘了把文件包传给我？"
 	exit 0
 fi
-cp "$1" ./
-rm -rf Assets.json
-unzip ./*.ipa > /dev/null
-mv ./Payload/DuoDuoFu.app/Assets.car ./
-sudo -S xcrun --sdk iphoneos assetutil --info ./Assets.car > ./Assets.json
-rm -rf Assets.car Payload
+rm -rf ./tmpabcipa
+rm -rf ./Assets.json
+mkdir ./tmpabcipa
+cp "$1" ./tmpabcipa/tmpabc.zip
+unzip ./tmpabcipa/tmpabc.zip -d ./tmpabcipa/ > /dev/null
+sudo -S xcrun --sdk iphoneos assetutil --info ./tmpabcipa/Payload/*.app/Assets.car > ./Assets.json
+rm -rf ./tmpabcipa
 echo "查找P3:"
-grep "P3" ./Assets.json > /dev/null
+grep "P3" ./Assets.json -i > /dev/null
 if [ $? -eq 0 ]; then
 	echo -e "[\033[31merror\033[0m]找到P3图请注意!!!"
+	grep "P3" ./Assets.json -ni
 else
 	echo -e "[\033[32mok\033[0m]未找到P3图"
 fi
 echo "查找16位图:"
-grep "\"BitsPerComponent\"\ :\ 16" ./Assets.json > /dev/null
+grep "\"BitsPerComponent\"\ :\ 16" ./Assets.json -i > /dev/null
 if [ $? -eq 0 ]; then
         echo -e "[\033[31merror\033[0m]找到16位图请注意!!!"
+	grep "\"BitsPerComponent\"\ :\ 16" ./Assets.json -ni
 else
         echo -e "[\033[32mok\033[0m]未找到16位图"
 fi
